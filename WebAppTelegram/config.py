771072e4dc8758f9e0941ce5758666b79fa8a3a3
FilinStelss <145925720+FilinStelss@users.noreply.github.com>
2025-03-22 23:32:35 +0300

    Delete WebAppTelegram directory

--- WebAppTelegram/config.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
-if not TELEGRAM_TOKEN:
-    raise ValueError("TELEGRAM_TOKEN не найден в файле .env!")
